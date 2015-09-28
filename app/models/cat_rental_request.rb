fd5e9b91b6736cead961ae0b3de948f5a7e490bb
Todd Ruhlen <rhythmtome@yahoo.com>
2015-09-28 18:03:38 -0400

    Add request approval and denial

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -4,7 +4,35 @@ class CatRentalRequest < ActiveRecord::Base
   validates :cat_id, :start_date, :end_date, :status, presence: true
   STATUSES = ["PENDING", "APPROVED", "DENIED"]
   validates :status, inclusion: { in: STATUSES }
-  validate :overlapping_approved_requests
+  validate :overlapping_approved_requests?
+
+  def approve!
+    transaction do
+      pending_request_overlaps = overlapping_pending_requests
+      unless pending_request_overlaps.nil?
+        pending_request_overlaps.each do |request|
+          request.update(status: "DENIED")
+        end
+      end
+      self.update(status: "APPROVED")
+    end
+  end
+
+  def overlapping_approved_requests?
+    if requests = overlapping_requests
+      requests.each do |request|
+        if request.approved?
+          errors.add(:base, "Another request has been approved from #{request.start_date} to #{request.end_date}")
+        end
+      end
+    end
+  end
+
+  def overlapping_pending_requests
+    overlapping_requests.select do |request|
+      request.pending?
+    end
+  end
 
   def overlapping_requests
     requests = CatRentalRequest.where(cat_id: self.cat_id)
@@ -25,17 +53,12 @@ class CatRentalRequest < ActiveRecord::Base
     end
   end
 
-  def overlapping_approved_requests
-    if requests = overlapping_requests
-      requests.each do |request|
-        if request.approved?
-          errors.add(:base, "Another request has been approved from #{request.start_date} to #{request.end_date}")
-        end
-      end
-    end
-  end
 
   def approved?
     status == "APPROVED"
+  end
+
+  def pending?
+    status == "PENDING"
   end
 end
